bc6e9538122b35189ea806a637c7bb89967cc3d1
andrepimenta <andrepimenta7@gmail.com>
2019-07-25 16:43:20 +0100

    added files

---
+++ ios/LoadingOverlay.h
@@ -0,0 +1,17 @@
+//
+//  LoadingOverlay.h
+//  ReatNativeLoadingSpinnerOverlayNativeTutorial
+//
+//  Created by Andre Pimenta on 25/07/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoadingOverlay : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/LoadingOverlay.m
@@ -0,0 +1,13 @@
+//
+//  LoadingOverlay.m
+//  ReatNativeLoadingSpinnerOverlayNativeTutorial
+//
+//  Created by Andre Pimenta on 25/07/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "LoadingOverlay.h"
+
+@implementation LoadingOverlay
+
+@end
